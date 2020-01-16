7e6ab3680f7f3fce26facf4885a1862002a3a6f7
Kerby <kerby.jean@hotmail.fr>
2020-01-15 19:16:20 -0500

    done

--- AirportDepartures.playground/Contents.swift
+++ AirportDepartures.playground/Contents.swift
@@ -63,23 +63,8 @@ class DepartureBoard {
             case .Canceled:
                 message = "We're sorry your flight to \(flight.airpot.destination) was canceled, here is a $500 voucher"
             }
-            
         }
     }
-    
-    // see the nearest information desk for more details
-    
-//    a. If the flight is canceled print out: "We're sorry your flight to (city) was canceled, here is a $500 voucher"
-//
-//    b. If the flight is scheduled print out: "Your flight to (city) is scheduled to depart at (time) from terminal: (terminal)"
-//
-//    c. If their flight is boarding print out: "Your flight is boarding, please head to terminal: (terminal) immediately. The doors are closing soon."
-//
-//    d. If the departureTime or terminal are optional, use "TBD" instead of a blank String
-//
-//    e. If you have any other cases to handle please print out appropriate messages
-//
-//    d. Call the alertPassengers() function on your DepartureBoard object below
 }
 
 
